a4f66b2a3faef76d06939275ceb149f6a47d71b1
thelscon <thelscon@gmail.com>
2021-07-25 16:19:05 +0300

    v5

--- script.js
+++ script.js
@@ -1,63 +1,66 @@
 'use strict' ;
 
-const PERMISSION_GANDER = [ 'male' , 'female' , 'non-binary' , 'transgender' , 'intersex' , 'n/a' ] ;
+const Person = function () {
+    const PERMISSION_GANDER = [ 'male' , 'female' , 'non-binary' , 'transgender' , 'intersex' , 'n/a' ] ;
 
-const secretFunctionName = function ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) value = currentValue.trim() ;
-        return value ;
-    } ;
-}
-const _name = secretFunctionName () ;
-
-const secretFunctionAge = function ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) {
-            currentValue = parseInt ( currentValue ) ;
-            if( currentValue > 0 && currentValue <= 120 ) {
-                value = currentValue ;
-            }
-        } 
-        return value ;
-    } ;
-}
-const _age = secretFunctionAge () ;
-
-const secretFunctionGander = function ( currentValue ) {
-    let value ;
-    return function ( currentValue ) { 
-        if ( currentValue !== undefined ) {
-            currentValue = currentValue.trim().toLowerCase() ;
-            if( PERMISSION_GANDER.includes( currentValue ) ) value = currentValue ;
-        } 
-        return value ;
-    } ;
-}
-const _gander = secretFunctionGander () ;
-
-const newPerson = Object.defineProperties ( {} , {
-    'name' : {
-        get : function () { return _name () } ,
-        set : function ( value ) { _name ( value ) } ,
-        enumerable : false ,
-        configurable : false
-    } ,
-    'age' : {
-        get : function () { return _age () } ,
-        set : function ( value ) { _age ( value ) } ,
-        enumerable : false ,
-        configurable : false
-    } ,
-    'gander' : {
-        get : function () { return _gander () } ,
-        set : function ( value ) { _gander ( value ) } ,
-        enumerable : false ,
-        configurable : false
+    const secretFunctionName = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) value = currentValue.trim() ;
+            return value ;
+        } ;
     }
-} ) ;
+    const _name = secretFunctionName () ;
 
+    const secretFunctionAge = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) {
+                currentValue = parseInt ( currentValue ) ;
+                if( currentValue > 0 && currentValue <= 120 ) {
+                    value = currentValue ;
+                }
+            } 
+            return value ;
+        } ;
+    }
+    const _age = secretFunctionAge () ;
+
+    const secretFunctionGander = function ( currentValue ) {
+        let value ;
+        return function ( currentValue ) { 
+            if ( currentValue !== undefined ) {
+                currentValue = currentValue.trim().toLowerCase() ;
+                if( PERMISSION_GANDER.includes( currentValue ) ) value = currentValue ;
+            } 
+            return value ;
+        } ;
+    }
+    const _gander = secretFunctionGander () ;
+
+    return Object.defineProperties ( {} , {
+        'name' : {
+            get : function () { return _name () } ,
+            set : function ( value ) { _name ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        } ,
+        'age' : {
+            get : function () { return _age () } ,
+            set : function ( value ) { _age ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        } ,
+        'gander' : {
+            get : function () { return _gander () } ,
+            set : function ( value ) { _gander ( value ) } ,
+            enumerable : false ,
+            configurable : false
+        }
+    } ) ;
+}
+
+const newPerson = Person () ;
 newPerson.name = ' bob' ;
 newPerson.gander = ' Male' ;
 newPerson.age = 120 ;
